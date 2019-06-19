a490eba1889e95f690232af1cc1a43440715d38a
Saif Allah Khaled <Saifallak@yahoo.com>
2019-06-19 16:00:24 +0200

    update Noah Scaffold

--- lib/ui/auth/reg/pin_code.dart
+++ lib/ui/auth/reg/pin_code.dart
@@ -9,51 +9,48 @@ class RegPinCode extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return NoahScaffold(
-      body: Container(
-        width: MediaQuery.of(context).size.width,
-        height: MediaQuery.of(context).size.height * 0.8,
-        child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: SecurePinCode(
-            upperTitle: TinyContainer(
-              text: "التأكيد الامني",
-              backgroundColor: Color(0xFF6D6DFF),
-              maxWidth: MediaQuery.of(context).size.width * 0.9,
-              textColor: Colors.white,
-              fontSize: 14,
-            ),
-            bottomTitle: TinyContainer(
-              text: "$error",
-              backgroundColor: Colors.green,
-              maxWidth: MediaQuery.of(context).size.width * 0.9,
-              textColor: Colors.black,
-              fontSize: 14,
-            ),
-            bottomSubTitle: TinyContainer(
-              text: "برجاء إدخال الرمز المكون من 4 أرقام",
-              backgroundColor: Colors.white24,
-              maxWidth: MediaQuery.of(context).size.width * 0.9,
-              textColor: Colors.white,
-              fontSize: 14,
-            ),
-            keyboardTextStyle: TextStyle(
-              color: Colors.green,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
-            pinCodeTextStyle: TextStyle(
-              color: Colors.red,
-              fontWeight: FontWeight.bold,
-              fontSize: 24,
-            ),
-            underlineBorderColor: Colors.black,
-            obscurePin: true,
-            // to make pin * instead of number
-            codeLength: 4,
-            correctPin: "****",
-            onCodeSuccess: (val) {},
-            onCodeFails: (val) {},
+      isScrollable: false,
+      body: Directionality(
+        textDirection: TextDirection.ltr,
+        child: SecurePinCode(
+          upperTitle: TinyContainer(
+            text: "التأكيد الامني",
+            backgroundColor: Color(0xFF6D6DFF),
+            maxWidth: MediaQuery.of(context).size.width * 0.9,
+            textColor: Colors.white,
+            fontSize: 14,
           ),
+          bottomTitle: TinyContainer(
+            text: "$error",
+            backgroundColor: Colors.green,
+            maxWidth: MediaQuery.of(context).size.width * 0.9,
+            textColor: Colors.black,
+            fontSize: 14,
+          ),
+          bottomSubTitle: TinyContainer(
+            text: "برجاء إدخال الرمز المكون من 4 أرقام",
+            backgroundColor: Colors.white24,
+            maxWidth: MediaQuery.of(context).size.width * 0.9,
+            textColor: Colors.white,
+            fontSize: 14,
+          ),
+          keyboardTextStyle: TextStyle(
+            color: Colors.green,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+          pinCodeTextStyle: TextStyle(
+            color: Colors.red,
+            fontWeight: FontWeight.bold,
+            fontSize: 24,
+          ),
+          underlineBorderColor: Colors.black,
+          obscurePin: true,
+          // to make pin * instead of number
+          codeLength: 4,
+          correctPin: "****",
+          onCodeSuccess: (val) {},
+          onCodeFails: (val) {},
         ),
       ),
     );
